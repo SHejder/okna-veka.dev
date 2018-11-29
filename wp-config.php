1383a77ec99b9db906917d6c855cb73c7a5578b0
SHejder <syhanov.m@yandex.ru>
2018-11-29 12:39:46 +0300

    add new asserts

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'u0267496_default' );
+define( 'DB_NAME', 'a0185522_okna-veka' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'a0185522_okna-veka' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'BCy7LS17' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
